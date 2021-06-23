e71a862efbbac17d28a34e7a245757f57b07bcb8
Ed Fricker <edafricker@gmail.com>
2021-06-23 12:05:03 +0100

    WIP

--- app.js
+++ app.js
@@ -1,14 +1,53 @@
 const http = require('http');
+const url = require('url');
 
 const port = process.env.PORT
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Convox!\nI\'m: ' + process.env.APP + '\nBuild:' + process.env.BUILD + '\nRelease: ' + process.env.RELEASE + '\nrunning on: ' + process.env.RACK);
-  console.log('PING');
+  console.log(JSON.stringify(req.headers));
+
+  timeout = 0
+  statusCode = 200
+  const path = url.parse(req.url, true).pathname
+  const queryObject = url.parse(req.url, true).query;
+  if (path == '/wait') {
+    timeout = queryObject.time;
+  }
+
+
+
+  linkvars = ""
+
+  for (const variable in process.env) {
+    if (variable.endsWith("_URL")) {
+      linkvars += variable + " : " + process.env[variable] + '\n'
+    }
+  }
+
+  responseText = 'Hello Convox!\nI\'m: ' + process.env.APP + '\\' + process.env.SERVICE + '\nBuild:' + process.env.BUILD + '\nRelease: ' + process.env.RELEASE + '\nrunning on: ' + process.env.RACK + '\n and Instance: ' + process.env.INSTANCE_IP + '\n and timeout: ' + timeout + '\n and path: ' + path + '\n and query: ' + JSON.stringify(queryObject) + `\n\n\n\nLink variables: \n` + linkvars;
+
+  if (path == '/failure') {
+    statusCode = 500;
+    responseText = '';
+  }
+
+  setTimeout(function () {
+    res.statusCode = statusCode;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(responseText);
+    console.log('PING');
+  }, timeout)
+
 });
 
 server.listen(port, () => {
-  console.log(`Server running at ${port}/`);
+  linkvars = ""
+
+  for (const variable in process.env) {
+    if (variable.endsWith("_URL")) {
+      linkvars += variable + " : " + process.env[variable] + '\n'
+    }
+  }
+
+  console.log(`Server running at ${port}/` + process.env.APP + '\\' + process.env.SERVICE + '\nBuild:' + process.env.BUILD + '\nRelease: ' + process.env.RELEASE + '\nrunning on: ' + process.env.RACK + '\n and Instance: ' + process.env.INSTANCE_IP + `\n\nLink variables: ` + linkvars);
 });
